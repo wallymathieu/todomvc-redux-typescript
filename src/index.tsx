13c1c5bb29a961d9b8f966c097d0b96bbaf308a2
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-01-06 11:44:12 +0100

    +Thunk middleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
 import 'todomvc-app-css/index.css'
-
-const store = createStore(reducer)
+import thunk from 'redux-thunk';
+const store = createStore(reducer,applyMiddleware(thunk))
 
 render(
   <Provider store={store}>
