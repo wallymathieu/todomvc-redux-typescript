d17baa947860e279394e744139d2c0c176e79ca8
Oskar Mathieu Gewalli <gewalli@gmail.com>
2025-11-18 19:23:41 +0000

    bump bump

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createRoot } from 'react-dom/client'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
 import 'todomvc-app-css/index.css'
 import { callAPIMiddleware } from './middlewares/callapimiddleware'
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(callAPIMiddleware)))
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(callAPIMiddleware),
+})
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+const container = document.getElementById('root')
+if (container) {
+  const root = createRoot(container)
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  )
+}
